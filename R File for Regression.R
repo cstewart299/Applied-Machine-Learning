51e3fbffaf8e1921088f13f28c37f37cb7f072cd
cstewart299 <162852587+cstewart299@users.noreply.github.com>
2024-03-25 21:44:26 -0400

    Add files via upload

---
+++ R File for Regression.R
@@ -0,0 +1,8 @@
+library(readr)
+
+library(nnet)
+
+training_data <- read.csv("C:/Users/cstew/OneDrive/Desktop/Syracuse/Summer 2023/IST 707/Final Project/Training Data.csv")
+
+#Need to change categorical variables into binary variables 
+
